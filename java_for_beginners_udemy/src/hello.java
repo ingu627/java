8ee4e5349022223dfc67f144ae1ddc52c46943ea
ingu627 <rjsdudans@naver.com>
2022-03-02 21:55:10 +0900

    start java in udemy

---
+++ java_for_beginners_udemy/src/hello.java
@@ -0,0 +1,15 @@
+public class hello {
+    public static void main(String[] args) {
+        char var1 = '9';
+        String var2 = "Hi my name is poeun";
+        boolean var3 = false;
+        int var4 = 900;
+        double var5 = 1.2345;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+    }
+}
