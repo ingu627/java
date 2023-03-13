d4fc2a450b051c150c23e9ac4e8d962e764c2c40
ingu627 <rjsdudans@naver.com>
2023-03-13 14:23:04 +0900

    SOLID 원칙 정리

---
+++ oop-spring-basic/week4/src/Week4.java
@@ -0,0 +1,7 @@
+public class Week4 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.printf("안녕 "+ a);
+    }
+}
--- oop2023/WEEK2/src/정현석.java
+++ oop2023/WEEK2/src/정현석.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Lab02_202224205 {
+public class 정현석 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
